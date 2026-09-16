657926d67672eb3211cc126292f51c176d1f808f
David M <labsfiap@fiap.com.br>
2026-09-16 20:28:01 -0300

    teste

---
+++ script.js
@@ -0,0 +1,26 @@
+// // alert('Aula de web')
+// // //UserN -> Username/NomeUsuario
+// // var UserN = prompt('Insira seu nome de usuário:');
+
+// // console.log('Nome inserido:', UserN);
+
+// // var titulo = document.getElementById('sim');
+
+// // titulo.textContent = `Olá, ${UserN}`
+
+// var num = prompt('Digite um número:');
+// var numC = parseInt(num);
+
+// alert(numC + 10);
+
+// var num2 = parseInt(prompt('digita outro:'));
+// alert(num2 + 10);
+
+var n1 = parseInt(prompt('Insira o primeiro numero:'));
+var n2 = parseInt(prompt('Insira o segundo numero:'));
+
+var soma = n1 + n2;
+var produto = n1 * n2;
+
+console.log('A soma dos dois numeros inseridos é:', soma);
+alert(`O produto dos dois numeros inseridos é: ${produto}`);
